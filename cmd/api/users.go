2d57f6ea5436e16dbc15e7122474d6e4588de31b
陳又音 <nan@chenyouyindeMacBook-Pro.local>
2026-07-03 12:35:49 +0800

    fix: fix workflow staticcheck

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"net/http"
 	"strconv"
@@ -163,32 +162,32 @@ func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Reque
 	}
 }
 
-func (app *application) userContextMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		idParam := chi.URLParam(r, "userID")
+// func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		idParam := chi.URLParam(r, "userID")
 
-		id, err := strconv.ParseInt(idParam, 10, 64)
-		if err != nil {
-			app.badRequestError(w, r, err)
-			return
-		}
+// 		id, err := strconv.ParseInt(idParam, 10, 64)
+// 		if err != nil {
+// 			app.badRequestError(w, r, err)
+// 			return
+// 		}
 
-		ctx := r.Context()
-		user, err := app.store.User.GetByID(ctx, id)
-		if err != nil {
-			switch {
-			case errors.Is(err, store.ErrNotFound):
-				app.notFoundError(w, r, err)
-			default:
-				app.internalServerError(w, r, err)
-			}
-			return
-		}
+// 		ctx := r.Context()
+// 		user, err := app.store.User.GetByID(ctx, id)
+// 		if err != nil {
+// 			switch {
+// 			case errors.Is(err, store.ErrNotFound):
+// 				app.notFoundError(w, r, err)
+// 			default:
+// 				app.internalServerError(w, r, err)
+// 			}
+// 			return
+// 		}
 
-		ctx = context.WithValue(ctx, userCtxKey, user)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
+// 		ctx = context.WithValue(ctx, userCtxKey, user)
+// 		next.ServeHTTP(w, r.WithContext(ctx))
+// 	})
+// }
 
 func (app *application) getUserFromCtx(r *http.Request) *store.User {
 	user, _ := r.Context().Value(userCtxKey).(*store.User)
