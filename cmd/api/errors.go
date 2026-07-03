79ec530a0d12bcc23100aadef4e28a566589b07e
陳又音 <nan@chenyouyindeMacBook-Pro.local>
2026-07-03 12:37:38 +0800

    fix: fix workflow staticcheck

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -34,13 +34,13 @@ func (app *application) unauthorizedError(w http.ResponseWriter, r *http.Request
 	writeJSONError(w, http.StatusUnauthorized, "unauthorized")
 }
 
-func (app *application) unauthorizedBasicError(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Warnw("unauthorized:", "method", r.Method, "path", r.URL.Path, "error", err)
+// func (app *application) unauthorizedBasicError(w http.ResponseWriter, r *http.Request, err error) {
+// 	app.logger.Warnw("unauthorized:", "method", r.Method, "path", r.URL.Path, "error", err)
 
-	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+// 	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
 
-	writeJSONError(w, http.StatusUnauthorized, "unauthorized")
-}
+// 	writeJSONError(w, http.StatusUnauthorized, "unauthorized")
+// }
 
 func (app *application) forbiddenError(w http.ResponseWriter, r *http.Request, err error) {
 	app.logger.Warnw("forbidden:", "method", r.Method, "path", r.URL.Path, "error", err)
